d022616d95d5f89aeec96cb08f221897636cdd76
superbrains <superbrains2005@yahoo.com>
2019-08-05 10:43:43 +0100

    Login, Forgot Password, Main Screen, Master/Detail/Templates

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,58 @@
-//
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Legalpedia Mobile App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'LEGAL PEDIA',
+                    style: TextStyle(
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                )
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
+}
