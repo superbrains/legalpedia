776ceec7b9f2067ca4ac221723728e9c269b0a25
superbrains <superbrains2005@yahoo.com>
2019-08-06 13:49:19 +0100

    Login, Forgot Password, Main Screen, Master/Detail/Templates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:ui' as prefix1;
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -8,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LEGALPEDIA',
       theme: ThemeData(
 
         primarySwatch: Colors.red,
@@ -33,115 +36,242 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    appBar: new AppBar(
+    elevation: 7.0,
+
+  title:  Image.asset('assets/LGPLogo.png', width: 200.0, height: 50.0,),
+      backgroundColor: Colors.red,
+
+),
+      drawer: new Drawer(
+        child: new Stack(
           children: <Widget>[
-            Container(
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                      child: Text(
-                        'LEGALPEDIA',
-                        style: TextStyle(
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    )
-                  ],
-                )
+            ClipPath(
+              child: Container(
+                color: Colors.red.withOpacity(1),
+
+              ),
+              clipper: getClipper(),
             ),
-            Container(
-              padding: EdgeInsets.only(top: 35.00, left: 20.0, right: 20.0),
+
+            Positioned(
+
+              width: 300.0,
+              top: MediaQuery.of(context).size.height/6.7,
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'REGISTERED USERNAME',
-                      labelStyle:  TextStyle(
-                        fontFamily: 'MontSerrat',
-                        fontWeight: FontWeight.bold
-                      )
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'REGISTERED EMAIL',
-                        labelStyle:  TextStyle(
-                            fontFamily: 'MontSerrat',
-                            fontWeight: FontWeight.bold
-                        )
-                    ),
-
-                  ),
-
-                  SizedBox(height: 40.0),
                   Container(
-                    height: 40.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.redAccent,
-                      color: Colors.red,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: (){},
-                        child: Center(
-                          child: Text(
-                            'REQUEST PASSWORD',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'MontSerrat'
-                            ),
-                          ),
-                        ),
-                      )
-                    ),
-
-                  ),
-                  SizedBox(height: 20.0),
-                  Container(
-                    height: 40.0,
-                    color: Colors.transparent,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all( color: Colors.black, style: BorderStyle.solid, width: 1.0),
-                        color: Colors.transparent,
-                        borderRadius:  BorderRadius.circular(20.0)
-
+                    width: 100.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      image: DecorationImage(
+                        image: NetworkImage('http://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w700.h700.jpg'),
+                        fit: BoxFit.cover
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                         children: <Widget>[
-                           Center(
-                             child: IconButton(icon: new Icon(Icons.arrow_back), onPressed: null),
-                           ),
-                           Center(
-                             child: Text('Back', style:  TextStyle(
-                              fontWeight:  FontWeight.bold,
-                                 fontFamily: 'MontSerrat'
-                             ),),
-                           )
-                         ],
-                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                     boxShadow: [
+                       BoxShadow(blurRadius: 20.0, color: Colors.black)
+                     ]
+                     // border: Border.all(color: Colors.black, width: 1.0, style: BorderStyle.solid)
                     ),
+
                   ),
-                  SizedBox(height: 100.0),
-                  Container(
-                    child: Text('Empowering you to achieve more...', style: TextStyle(
-                      fontWeight:  FontWeight.bold,
-                      color: Colors.redAccent
-                    ),),
-                  )
+                  SizedBox(height: 10),
+                  Text('Obi Azubike',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontFamily: 'Monseratti',
+                    fontWeight: FontWeight.bold
+                  ),),
+                  SizedBox(height: 5),
+                  Text('License Expires on the 5th of September, 2019',
+                    style: TextStyle(
+                        fontSize: 12.0,
+                        fontFamily: 'Monseratti',
+                        fontStyle: FontStyle.italic
+                    ),)
+
+
+
                 ],
+
+
               ),
 
-    )
+            ),
+
+            Positioned(
+              top: MediaQuery.of(context).size.height/2.1,
+              width: 300.0,
+
+              child: new Column(
+                children: <Widget>[
+                  CustomListTile(Icons.help_outline, 'Ask a Legalpedia Lawyer', ()=>{}),
+                  CustomListTile(Icons.account_box, 'About Legapedia', ()=>{}),
+                  CustomListTile(Icons.feedback, 'Feedback', ()=>{}),
+                  CustomListTile(Icons.face, 'Visit Our Facebook Page', ()=>{}),
+                  CustomListTile(Icons.transfer_within_a_station, 'Visit Our Twitter Page', ()=>{}),
+                  CustomListTile(Icons.lock, 'Logout', ()=>{}),
+                ],
+              ),
+            )
+
           ],
-        )
+
+
+        ),
+
+
+      ),
+
+    //  backgroundColor: Color.alphaBlend(Colors.redAccent,  Colors.redAccent),
+
+      body: new  ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    color: Colors.red,
+
+                  ),
+                SizedBox(height:  25.0),
+        Padding(
+          padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 30),
+          child: Text('Find authorities on any issue of law in the search box', style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.normal,
+
+
+          ), textAlign: TextAlign.center
+            ,),
+        ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 80),
+                  child: Material(
+                    elevation: 7.0,
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search, color: Colors.red, size: 30.0,),
+                        contentPadding: EdgeInsets.only(left: 15.0, right: 15.0, top:15.0),
+                        hintText: 'Search'
+                      ),
+                    ),
+                  ),
+
+                )
+                /*  Positioned(
+                    bottom: 10,
+                    right: 100,
+                    child: Container(
+                      height: 400,
+                      width: 400,
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(100.0),
+                         color: Colors.red.shade400
+                       ),
+                    ),
+                  )*/
+                ],
+              ),
+              SizedBox(height: 40.0,),
+              Stack(
+                children: <Widget>[
+                  Row(
+
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                    Container(
+
+                    )
+
+                    ],
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+
+      ),
+
     );
+
+  }
+}
+
+class CustomListTile extends StatelessWidget{
+  IconData icon;
+  String text;
+  Function onTap;
+
+  CustomListTile(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+return Padding(
+  padding: EdgeInsets.all(6.0),
+  child: Container(
+    decoration: BoxDecoration(
+      border: Border( bottom: BorderSide(color: Colors.grey.shade400)),
+
+    ),
+    child: InkWell(
+
+      splashColor: Colors.redAccent,
+      onTap: onTap,
+
+      child: Container(
+        height: 40.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Icon(icon, color: Colors.red),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  child:   Text(text, style: TextStyle(
+                      fontSize: 16.0
+                  ),) ,
+                )
+
+              ],
+            ),
+
+            Icon(Icons.arrow_right)
+          ],
+        ),
+      ),
+
+    ) ,
+  ),
+
+);
+
+  }
+}
+
+class getClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size){
+    var path = new Path();
+
+    path.lineTo(0.0, size.height/3);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<prefix1.Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
   }
 }
