dd0428562300ecd7b8757fbc98f7559942a78bbe
superbrains <superbrains2005@yahoo.com>
2019-09-09 12:36:23 +0100

    Login, Forgot Password, Main Screen, Master/Detail/Templates, Added Fonts

---
+++ lib/globals.dart
@@ -0,0 +1,7 @@
+import 'package:legalpedia/models/ratiosmodel.dart';
+import 'package:legalpedia/models/summarymodel.dart';
+
+bool isLoggedIn = false;
+
+List<RatioModel> ratios = List();
+List<SummaryModel> summary = List();
